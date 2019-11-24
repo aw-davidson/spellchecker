ca3528d08df8b40422ae3745039c29ee8a1c2592
Alec Davidson <alec.davidson@adp.com>
2019-11-23 19:59:04 -0500

    feature: add timer in main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,33 @@
 #include <iostream>
 #include <unordered_set>
 #include "SpellChecker/SpellChecker.h"
+#include <chrono> 
+using namespace std::chrono; 
+  
 
 
 int main() {
   try 
   {
+
     SpellChecker checker("src/utils/word_freq.txt");
 
+    auto start = high_resolution_clock::now(); 
     std::cout << checker.correction("expresion") << std::endl;
     std::cout << checker.correction("thea") << std::endl;
     std::cout << checker.correction("helk") << std::endl;
     std::cout << checker.correction("javaf") << std::endl;
-    std::cout << checker.correction("programser") << std::endl;
+    std::cout << checker.correction("javasipt") << std::endl;
+    std::cout << checker.correction("queot") << std::endl;
+    std::cout << checker.correction("hous") << std::endl;
+    std::cout << checker.correction("cropg") << std::endl;
+    std::cout << checker.correction("velluam") << std::endl;
+    std::cout << checker.correction("haiir") << std::endl;
+    auto stop = high_resolution_clock::now(); 
+
+    auto duration = duration_cast<microseconds>(stop - start); 
+  
+    std::cout << "This run took " << duration.count() << " microseconds" << std::endl; 
   }
   catch(const std::runtime_error& re)
   {
